2860f5ad83d01e535c19a65fb02fc579db598d25
Achyut <onestring22@gmail.com>
2024-11-03 21:33:09 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
-    domains:["images.unsplash.com", "images.pexels.com"],
-    unoptimized: true,
-  },
-  basePath: '/UI-Nextjs',
+    domains:["images.unsplash.com", "images.pexels.com"]
+  }
 };
 
 export default nextConfig;
