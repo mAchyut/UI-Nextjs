4c110a4fc2d94ba677bb65c0d875bdf5331014e5
Achyut <onestring22@gmail.com>
2024-11-03 23:15:23 +0530

    next.config.js replace domain from remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images:{
-    domains:["images.unsplash.com", "images.pexels.com"]
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**", // optional: allows all paths
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "/**", // optional: allows all paths
+      },
+    ],
+  },
 };
 
 export default nextConfig;
