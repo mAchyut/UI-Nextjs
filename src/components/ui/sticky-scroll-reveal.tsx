e0244d7c5a3c72d0db0b06f90d3024cd560a5d08
Achyut <onestring22@gmail.com>
2024-11-05 14:53:09 +0530

    fixes in const

--- src/components/ui/sticky-scroll-reveal.tsx
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -50,7 +50,7 @@ export const StickyScroll = ({
 
   useEffect(() => {
     setBackgroundGradient(linearGradients[activeCard % linearGradients.length]);
-  }, [activeCard]);
+  }, [activeCard, linearGradients]);
 
   return (
     <motion.div
