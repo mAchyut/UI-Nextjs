a4c4c8403a66499a3b93f3dea6c1a6c2a91c298d
Achyut <onestring22@gmail.com>
2024-11-05 15:04:23 +0530

    navbar changes seslect-none

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,16 +9,16 @@ function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
 
     return (
-        <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+        <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 select-none", className)}>
             <Menu setActive={setActive}>
                 <Link href={"/"} passHref>
                     <MenuItem setActive={setActive} active={active} item="Home" />
                 </Link>
 
-                <MenuItem setActive={setActive} active={active} item="Our services">
+                <MenuItem setActive={setActive} active={active} item="Services">
                     <div className="flex flex-col space-y-4 text-red-600">
-                        <HoveredLink href="/courses">Check courses</HoveredLink>
-                        <HoveredLink href="/design-principles">Design principles</HoveredLink>
+                        <HoveredLink href="/courses">Check-courses</HoveredLink>
+                        <HoveredLink href="/design-principles">Design-principles</HoveredLink>
                         <HoveredLink href="/blogs">Blogs</HoveredLink>
                         <HoveredLink href="/docs">Docs</HoveredLink>
                     </div>
